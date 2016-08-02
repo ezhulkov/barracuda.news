e1eb0a9586bc74664ac3293d5d697d75df940035
ezhulkov <ezhulkov@gmail.com>
2016-08-02 20:54:38 +0300

    - rythm in progress

--- conf/compass/config.rb
+++ conf/compass/config.rb
@@ -2,7 +2,7 @@ require 'compass/import-once/activate'
 
 http_path = "/"
 css_dir = "../../public/stylesheets"
-sass_dir = "../../app/assets/sass"
+sass_dir = "../../app/assets/stylesheets"
 images_dir = "../../public/images"
 javascripts_dir = "../../public/javascripts"
 line_comments = false
