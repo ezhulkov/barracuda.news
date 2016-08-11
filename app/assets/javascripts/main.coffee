954838ffadc908d891cd85f571119db5bccb2550
ezhulkov <ezhulkov@gmail.com>
2016-08-11 21:23:25 +0300

    - admin in progress

--- app/assets/javascripts/main.coffee
+++ app/assets/javascripts/main.coffee
@@ -7,7 +7,7 @@ $(window).load ->
     spacing: "margin"
   })
 
-app = angular.module "barracudaApp", []
+app = angular.module "barracudaApp", ["bw.paging"]
 app.directive "autoFocus", ($timeout) ->
   return link: (scope, element, attrs) ->
     attrs.$observe("autoFocus", (newValue) ->
@@ -38,3 +38,14 @@ app.controller "MainController", ($timeout, $window, $scope, $http) ->
     $scope.searchOn = !$scope.searchOn
   $scope.toggleMenu = ->
     $scope.menuOn = !$scope.menuOn
+
+app.controller "AdminController", ($timeout, $window, $scope, $http) ->
+  $scope.news = $window.news
+  $scope.total = $scope.news.length
+  $scope.page = 1
+  $scope.pageSize = 4
+  $scope.newsPage = $scope.news.slice(0, $scope.pageSize)
+  $scope.showPage = (p) ->
+    from = (p - 1) * $scope.pageSize
+    $scope.newsPage = $scope.news.slice(from, from + $scope.pageSize);
+    return false
--- app/controllers/AdminController.scala
+++ app/controllers/AdminController.scala
@@ -2,7 +2,7 @@ package controllers
 
 import javax.inject._
 import controllers.actions.LoggingAction
-import models.NewsModel.{Layout, PeaceOfNews}
+import models.NewsModel.Layout
 import play.api.Environment
 import play.api.libs.json.Json
 import play.api.mvc._
@@ -17,11 +17,12 @@ class AdminController @Inject()(
   import models.Implicits.JsonImplicits._
   import scala.concurrent.ExecutionContext.Implicits.global
 
-  private val sampleNews = Json.parse(layoutContentStr("news")).as[Seq[PeaceOfNews]].flatMap(t => t.tags.map(tag => tag -> t)).groupBy { case (t, n) => t }.map { case (t, v) => t -> v.map(k => k._2) }
+  private val sampleNews = layoutContentStr("news")
   def layoutContentStr(name: String) = env.resourceAsStream(s"layouts/$name.json").map(is => Source.fromInputStream(is).mkString).getOrElse(throw new RuntimeException(s"no $name layout"))
   def parsedLayout(name: String) = name -> Json.parse(layoutContentStr(name)).as[Layout]
 
   def index = LoggingAction.async { implicit request => Future(Ok(views.html.admin.index(sampleNews))) }
   def layout = LoggingAction.async { implicit request => Future(Ok(views.html.admin.layouts())) }
+  def article = LoggingAction.async { implicit request => Future(Ok(views.html.admin.article())) }
 
 }
