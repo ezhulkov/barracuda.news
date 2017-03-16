4c2aac2aff8ec8e2c8717e6a2a81b50541fbfa85
ezhulkov <ezhulkov@gmail.com>
2017-03-16 13:07:23 +0300

    - gallery almost done

--- public/alloy-editor/plugins/gallery_admin/plugin.js
+++ public/alloy-editor/plugins/gallery_admin/plugin.js
@@ -6,8 +6,15 @@
             });
             if (insideGallery == undefined) {
                 editor.execCommand('enter');
+            } else {
+                var galleryItem = editor.getSelection().getStartElement().getAscendant(function (el) {
+                    return typeof el.hasClass === 'function' && el.hasClass('item_wrap');
+                });
+                if (galleryItem != undefined) {
+                    var newItem = CKEDITOR.dom.element.createFromHtml("<div class='item_wrap'><div class='item'><div class='preview'>&nbsp;</div><div class='caption'></div></div></div>");
+                    newItem.insertAfter(galleryItem);
+                }
             }
-            return;
         }
     };
     var pluginName = 'gallery_admin';
